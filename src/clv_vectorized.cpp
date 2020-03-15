0d221e5b6f9aee65a1828d42d7abbcb2b4bf12ff
Patrik <patrik.schilter@gmail.com>
2020-03-15 15:26:12 +0100

    export gsl hyp2f1 for usage in R code

--- src/clv_vectorized.cpp
+++ src/clv_vectorized.cpp
@@ -1,16 +1,43 @@
-
-
 #include <RcppArmadillo.h>
 #include <RcppGSL.h>
 #include <gsl/gsl_sf_hyperg.h>
+#include <gsl/gsl_sf_result.h>
+
+
+
+//' @description Calculate the hypergeometric 2f1 using the GSL library (gsl_sf_hyperg_2F1_e)
+//' @return List with value and status
+// [[Rcpp::export]]
+Rcpp::List vec_gsl_hyp2f1_e(const RcppGSL::Vector& vA, const RcppGSL::Vector& vB, const RcppGSL::Vector& vC, const RcppGSL::Vector& vZ){
+
+  if((vA->size != vB->size) || (vB->size != vC->size) || (vC->size != vZ->size))
+    throw std::runtime_error(std::string("Not all vectors are of the same length!"));
+
+  // Do not abort in case of error
+  gsl_set_error_handler_off();
+
+  const size_t n = vA->size;
+
+  RcppGSL::Vector vRes(n);
+  RcppGSL::IntVector vStatus(n);
+  gsl_sf_result gsl_res;
+
+  for(size_t i = 0; i<n; i++){
+    vStatus[i] = gsl_sf_hyperg_2F1_e(vA[i], vB[i], vC[i], vZ[i], &gsl_res);
+    vRes[i] = gsl_res.val;
+    // gsl_res.err
+  }
+
+  return Rcpp::List::create(Rcpp::Named("res") = Rcpp::wrap(vRes),
+                            Rcpp::Named("status") = Rcpp::wrap(vStatus));
+}
 
 
 namespace clv{
 
-
 // vec_hyp2F1 --------------------------------------------------
 //    All params as same-length vectors
-//
+
 arma::vec vec_hyp2F1(const arma::vec& vA, const arma::vec& vB, const arma::vec& vC, const arma::vec& vX){
 
   arma::vec vRes(vA);
