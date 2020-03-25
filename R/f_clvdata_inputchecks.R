07f23af65b2f57e04b9c54be96d9fac391f851fc
Patrik <patrik.schilter@gmail.com>
2020-03-26 00:50:11 +0100

    rename files: check inputs, plot, pnbddyncov
    move some functions

--- R/f_clvdata_inputchecks.R
+++ R/f_clvdata_inputchecks.R
@@ -1,3 +1,46 @@
+# function to to check if there are error messages and print+stop them
+check_err_msg <- function(err.msg){
+  if(length(err.msg) > 0)
+    stop(c("\n",paste0(err.msg, sep="\n")),call. = FALSE)
+}
+
+
+
+.check_user_data_single_boolean <- function(b, var.name){
+  err.msg <- c()
+  if(!is.logical(b))
+    return(paste0("The parameter ", var.name, " needs to be of type logical (True/False)!"))
+  if(length(b)>1)
+    err.msg <- c(err.msg, paste0("The parameter ", var.name, " can only contain a single element!"))
+  if(anyNA(b))
+    err.msg <- c(err.msg, paste0("The parameter ", var.name, " cannot be NA!"))
+  return(err.msg)
+}
+
+.check_userinput_single_character <- function(char, var.name){
+  err.msg <- c()
+  if(!is.character(char))
+    return(paste0(var.name, " needs to be of type character (text)!"))
+  if(length(char) != 1)
+    err.msg <- c(err.msg, paste0(var.name, " must contain exactly one single element!"))
+  if(anyNA(char))
+    err.msg <- c(err.msg, paste0(var.name, " may not contain any NA!"))
+  if(length(err.msg) == 0){
+    # is non empty vec, but check is not no text ("")
+    if(nchar(char[[1]]) == 0)
+      err.msg <- c(err.msg, paste0(var.name, " may not be empty text!"))
+  }
+
+  return(err.msg)
+}
+
+
+.convert_userinput_dataid <- function(id.data){
+  return(as.character(id.data))
+}
+
+
+
 #' @include clv_helpers.R
 check_userinput_datanocov_columnname <- function(name.col, data){
 
--- R/f_clvfitted_checkuserinput.R
+++ R/f_clvfitted_checkuserinput.R
@@ -1,38 +1,4 @@
-# function to to check if there are error messages and print+stop them
-check_err_msg <- function(err.msg){
-  if(length(err.msg) > 0)
-    stop(c("\n",paste0(err.msg, sep="\n")),call. = FALSE)
-}
 
-
-
-.check_user_data_single_boolean <- function(b, var.name){
-  err.msg <- c()
-  if(!is.logical(b))
-    return(paste0("The parameter ", var.name, " needs to be of type logical (True/False)!"))
-  if(length(b)>1)
-    err.msg <- c(err.msg, paste0("The parameter ", var.name, " can only contain a single element!"))
-  if(anyNA(b))
-    err.msg <- c(err.msg, paste0("The parameter ", var.name, " cannot be NA!"))
-  return(err.msg)
-}
-
-.check_userinput_single_character <- function(char, var.name){
-  err.msg <- c()
-  if(!is.character(char))
-    return(paste0(var.name, " needs to be of type character (text)!"))
-  if(length(char) != 1)
-    err.msg <- c(err.msg, paste0(var.name, " must contain exactly one single element!"))
-  if(anyNA(char))
-    err.msg <- c(err.msg, paste0(var.name, " may not contain any NA!"))
-  if(length(err.msg) == 0){
-    # is non empty vec, but check is not no text ("")
-    if(nchar(char[[1]]) == 0)
-      err.msg <- c(err.msg, paste0(var.name, " may not be empty text!"))
-  }
-
-  return(err.msg)
-}
 
 .check_user_data_single_numeric <- function(n, var.name){
   err.msg <- c()
@@ -44,12 +10,6 @@ check_err_msg <- function(err.msg){
     err.msg <- c(err.msg, paste0(var.name," may not be NA!"))
   return(err.msg)
 }
-
-
-.convert_userinput_dataid <- function(id.data){
-  return(as.character(id.data))
-}
-
 
 
 # Can be
