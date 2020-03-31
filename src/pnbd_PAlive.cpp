345aecefe057d1809d99bd64c658d74debe03ab4
Patrik <patrik.schilter@gmail.com>
2020-04-01 01:44:19 +0200

    formatting for palive

--- src/pnbd_PAlive.cpp
+++ src/pnbd_PAlive.cpp
@@ -24,8 +24,8 @@
 //'
 //' @details
 //' \code{vEstimated_params} vector with the estimated parameters in original scale
-//' for the Pareto/NBD model, namely (r, alpha, s, beta).
-//' r and alpha: unobserved parameters that describe the NBD transaction process.
+//' for the Pareto/NBD model, namely (r, alpha, s, beta). \cr
+//' r and alpha: unobserved parameters that describe the NBD transaction process. \cr
 //' s and beta: unobserved parameters that describe the pareto
 //' (exponential gamma) lifetime process.
 //'
