6857eedcf783671bcd16b1da696d84fa1a44a160
Patrik <patrik.schilter@gmail.com>
2020-05-05 15:15:02 +0200

    pnbd url references

--- man-roxygen/template_pnbd_reference.R
+++ man-roxygen/template_pnbd_reference.R
@@ -4,9 +4,9 @@
 #'
 #' Fader PS, Hardie BGS (2005). "A Note on Deriving the Pareto/NBD Model and
 #' Related Expressions."
-#' URL http://www.brucehardie.com/notes/009/pareto_nbd_derivations_ 2005-11-05.pdf.
+#' URL \url{http://www.brucehardie.com/notes/009/pareto_nbd_derivations_2005-11-05.pdf}.
 #'
 #' Fader PS, Hardie BG (2007). "Incorporating time-invariant covariates into the
 #' Pareto/NBD and BG/NBD models."
-#' URL http://www.brucehardie.com/notes/019/time_invariant_ covariates.pdf.
+#' URL \url{http://www.brucehardie.com/notes/019/time_invariant_covariates.pdf}.
 
