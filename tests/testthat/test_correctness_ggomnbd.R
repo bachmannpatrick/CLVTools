a81be5cc0f924667ce03c67c48f92bf870295724
Patrik <patrik.schilter@gmail.com>
2020-06-08 18:51:11 +0200

    single model testing methods: correctness

--- tests/testthat/test_correctness_ggomnbd.R
+++ tests/testthat/test_correctness_ggomnbd.R
@@ -1,20 +1,20 @@
-# Load required data ---------------------------------------------------------------------------------
 data("cdnow")
+data("apparelTrans")
+data("apparelStaticCov")
 
-set.seed(1000)
+
+fct.testthat.correctness(name.model = "GGompertz/NBD", method=ggomnbd, data.cdnow=cdnow,
+                         data.apparelTrans=apparelTrans, data.apparelStaticCov=apparelStaticCov,
+                         correct.start.params.model = c(r = 0.5, alpha = 2, b = 0.5, s = 0.5, beta = 0.5),
+                         correct.params.nocov.coef = c(r = 0.553, alpha = 10.578, b = 0.0002, s = 0.603, beta = 0.0026),
+                         correct.LL.nocov = -9377.94)
+
+# *** ADD comparison vs BTYD
+
 
 context("Correctness - GGompertz/NBD nocov - Recover parameters")
 
-
-expect_silent(clv.cdnow <- clvdata(data.transactions = cdnow,
-                                   date.format = "ymd",
-                                   time.unit = "W",
-                                   estimation.split = 38,
-                                   name.id = "Id",
-                                   name.date = "Date",
-                                   name.price = "Price"))
-
-# ** CROSS check with paper!
+# # ** CROSS check with paper!
 fct.testthat.correctness.nocov.correct.coefs(method = ggomnbd,
                                              cdnow = cdnow,
                                              start.params.model = c(r = 0.5, alpha = 2, b = 0.5, s = 0.5, beta = 0.5),
@@ -27,49 +27,3 @@ fct.testthat.correctness.nocov.correct.coefs(method = ggomnbd,
 #                                          start.params.model = c(r = 0.5, alpha = 2, b = 0.5, s = 0.5, beta = 0.5),
 #                                          params.nocov.se = c(r = 0.049, alpha = 0.949, b = 0.0000, s = 0.039, beta = 0.0004))
 
-# No cov: Predict ----------------------------------------------------------------------------------------------
-context("Correctness - GGompertz/NBD nocov - predict")
-
-fct.testthat.correctness.common.newdata.same.predicting.fitting(clv.fitted = ggomnbd(clv.data = clv.cdnow),
-                                                                clv.newdata = clv.cdnow)
-
-fct.testthat.correctness.nocov.newdata.fitting.sample.predicting.full.data.equal(method = ggomnbd,
-                                                                                 cdnow = cdnow,
-                                                                                 clv.cdnow = clv.cdnow)
-
-
-# Static cov: Data sorting ------------------------------------------------------------------------------
-context("Correctness - GGompertz/NBD static cov - Data sorting")
-
-data("apparelTrans")
-data("apparelStaticCov")
-
-expect_silent(clv.apparel <- clvdata(data.transactions = apparelTrans, date.format = "ymd", time.unit = "W",
-                                     estimation.split = 52))
-# Standard cov data
-expect_silent(clv.apparel.staticcov <- SetStaticCovariates(clv.apparel,
-                                                           names.cov.life = c("Gender", "Channel"), names.cov.trans = c("Gender", "Channel"),
-                                                           data.cov.life = apparelStaticCov, data.cov.trans = apparelStaticCov))
-
-expect_silent(g.static <- ggomnbd(clv.data=clv.apparel.staticcov, verbose=FALSE))
-
-fct.testthat.correctness.staticcov.sorted.covariates(method = ggomnbd,
-                                                     clv.apparel = clv.apparel,
-                                                     apparelStaticCov = apparelStaticCov,
-                                                     m.static = g.static)
-
-# Static cov: predict ---------------------------------------------------------------------------------------
-
-context("Correctness - GGompertz/NBD static cov - predict")
-
-fct.testthat.correctness.common.newdata.same.predicting.fitting(clv.fitted = ggomnbd(clv.cdnow),
-                                                                clv.newdata = clv.cdnow)
-
-fct.testthat.correctness.staticcov.fitting.sample.predicting.full.data.equal(method = ggomnbd,
-                                                                             apparelTrans = apparelTrans,
-                                                                             apparelStaticCov = apparelStaticCov,
-                                                                             clv.apparel.staticcov = clv.apparel.staticcov)
-
-fct.testthat.correctness.staticcov.regularization.lambda.0.no.regularization(method = ggomnbd,
-                                                                             clv.apparel.staticcov = clv.apparel.staticcov,
-                                                                             m.static = g.static)
--- tests/testthat/test_correctness_pnbd.R
+++ tests/testthat/test_correctness_pnbd.R
@@ -1,17 +1,17 @@
-# Recover parameters ---------------------------------------------------------------------------------
-context("Correctness - PNBD nocov - Recover parameters")
 data("cdnow")
+data("apparelTrans")
+data("apparelStaticCov")
 
-# Is used at various places
-expect_silent(clv.cdnow <- clvdata(data.transactions = cdnow, date.format = "ymd", time.unit = "W",
-                                   estimation.split = 38))
+# Correct coefs are our estimates
+fct.testthat.correctness(name.model = "PNBD", method=pnbd, data.cdnow=cdnow,
+                         data.apparelTrans=apparelTrans, data.apparelStaticCov=apparelStaticCov,
+                         correct.start.params.model = c(r=1, alpha = 1, s = 1, beta = 1),
+                         correct.params.nocov.coef = c(r=0.55315,   alpha=10.57633,  s=0.60625,   beta=11.67150),
+                         correct.LL.nocov = -9594.976)
 
-# Our estimates
-fct.testthat.correctness.nocov.correct.coefs(method = pnbd,
-                                             cdnow = cdnow,
-                                             start.params.model = c(r=1, alpha = 1, s = 1, beta = 1),
-                                             params.nocov.coef = c(r=0.55315,   alpha=10.57633,  s=0.60625,   beta=11.67150),
-                                             LL.nocov = -9594.976)
+
+# # Recover parameters ---------------------------------------------------------------------------------
+context("Correctness - PNBD nocov - Recover parameters")
 
 # As also reported to compare against bgnbd in Fader, Hardie, Lee (2005)
 fct.testthat.correctness.nocov.correct.coefs(method = pnbd,
@@ -19,13 +19,10 @@ fct.testthat.correctness.nocov.correct.coefs(method = pnbd,
                                              start.params.model = c(r=1, alpha = 1, s = 1, beta = 1),
                                              params.nocov.coef = c(r=0.553,   alpha=10.578,  s=0.606,   beta=11.669),
                                              LL.nocov = -9595.0)
-
-
 fct.testthat.correctness.nocov.correct.se(method = pnbd,
                                           cdnow = cdnow,
                                           start.params.model = c(r=1, alpha = 2, s = 1, beta = 2),
                                           params.nocov.se = c(r=0.0476264, alpha=0.8427222, s=0.1872594, beta=6.2105448))
-
 
 fct.testthat.correctness.nocov.same.as.btyd(clvtools.method = pnbd,
                                             btyd.method = BTYD::pnbd.EstimateParameters,
@@ -34,46 +31,7 @@ fct.testthat.correctness.nocov.same.as.btyd(clvtools.method = pnbd,
                                             btyd.palive.method = BTYD::pnbd.PAlive,
                                             start.params.model = c(r=0.5, alpha = 6, s = 0.9, beta = 8), cdnow = cdnow)
 
-# No cov: Predict ----------------------------------------------------------------------------------------------
-context("Correctness - PNBD nocov - predict")
-fct.testthat.correctness.common.newdata.same.predicting.fitting(clv.fitted = pnbd(clv.cdnow),
-                                                                clv.newdata = clv.cdnow)
+# **TODO: compare CBS vs BTYD
+# context("Correctness - BGNBD nocov - CBS comparison")
+# fct.testthat.correctness.nocov.compare.cbs.vs.btyd(cdnow = cdnow)
 
-fct.testthat.correctness.nocov.newdata.fitting.sample.predicting.full.data.equal(method = pnbd,
-                                                                                 cdnow = cdnow,
-                                                                                 clv.cdnow = clv.cdnow)
-
-# Static cov: Data sorting ------------------------------------------------------------------------------
-context("Correctness - PNBD static cov - Data sorting")
-
-data("apparelTrans")
-data("apparelStaticCov")
-
-expect_silent(clv.apparel <- clvdata(data.transactions = apparelTrans, date.format = "ymd", time.unit = "W",
-                                     estimation.split = 52))
-# Standard cov data
-expect_silent(clv.apparel.staticcov <- SetStaticCovariates(clv.apparel,
-                                                           names.cov.life = c("Gender", "Channel"), names.cov.trans = c("Gender", "Channel"),
-                                                           data.cov.life = apparelStaticCov, data.cov.trans = apparelStaticCov))
-
-expect_silent(p.static <- pnbd(clv.data=clv.apparel.staticcov, verbose=FALSE))
-
-fct.testthat.correctness.staticcov.sorted.covariates(method = pnbd,
-                                                     clv.apparel = clv.apparel,
-                                                     apparelStaticCov = apparelStaticCov,
-                                                     m.static = p.static)
-
-# Static cov: predict ---------------------------------------------------------------------------------------
-context("Correctness - PNBD static cov - predict")
-
-fct.testthat.correctness.common.newdata.same.predicting.fitting(clv.fitted = p.static,
-                                                                clv.newdata = clv.apparel.staticcov)
-
-fct.testthat.correctness.staticcov.fitting.sample.predicting.full.data.equal(method = pnbd,
-                                                                             apparelTrans = apparelTrans,
-                                                                             apparelStaticCov = apparelStaticCov,
-                                                                             clv.apparel.staticcov = clv.apparel.staticcov)
-
-fct.testthat.correctness.staticcov.regularization.lambda.0.no.regularization(method = pnbd,
-                                                                             clv.apparel.staticcov = clv.apparel.staticcov,
-                                                                             m.static = p.static)
