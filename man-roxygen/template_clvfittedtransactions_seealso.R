a8c85469a21f220e4f1521b8dfe3accf41d2d1c9
Patrik <patrik.schilter@gmail.com>
2020-07-26 00:16:51 +0200

    fix checks

--- man-roxygen/template_clvfittedtransactions_seealso.R
+++ man-roxygen/template_clvfittedtransactions_seealso.R
@@ -2,5 +2,5 @@
 #' to add static covariates to an existing clv data object.
 #'
 #' @seealso \code{\link[CLVTools:predict.clv.fitted.transactions]{predict}} to predict expected transactions, probability of being alive, and customer lifetime value for every customer
-#' @seealso \code{\link[CLVTools:plot.clv.fitted]{plot}} to plot the unconditional expectation as predicted by the fitted model
+#' @seealso \code{\link[CLVTools:plot.clv.fitted.transactions]{plot}} to plot the unconditional expectation as predicted by the fitted model
 #' @seealso The generic functions \code{\link[CLVTools:vcov.clv.fitted]{vcov}}, \code{\link[CLVTools:summary.clv.fitted]{summary}}, \code{\link[CLVTools:fitted.clv.fitted]{fitted}}.
