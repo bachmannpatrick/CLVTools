22ac9b8e239ea6881e57604f9bce04b2acf490e8
Patrik <patrik.schilter@gmail.com>
2020-07-30 23:56:55 +0200

    Revert "gg: block prices=0"
    This reverts commit e42239072484b9c40a6c8daf8731ad0b076e2616.

--- tests/testthat/test_inputchecks_gg.R
+++ tests/testthat/test_inputchecks_gg.R
@@ -14,16 +14,17 @@ fct.testthat.inputchecks.clvfittedspending.nocov(name.method = "Gamma-Gamma", me
                                                  data.cdnow = cdnow)
 
 
-test_that("Cannot fit on data with negative or zero spending", {
+test_that("Cannot fit on data with negative spending", {
   cdnow.neg <- copy(cdnow)
 
-  expect_silent(cdnow.neg[1000, Price := 0])
-  expect_silent(clv.neg <- clvdata(cdnow.neg, date.format = "ymd", time.unit = "w", estimation.split = 37))
-  expect_error(gg(clv.neg), regexp = "zero")
+  # TODO: Cann fit on Prices = 0?
+  # expect_silent(cdnow.neg[1000, Price := 0])
+  # expect_silent(clv.neg <- clvdata(cdnow.neg, date.format = "ymd", time.unit = "w", estimation.split = 37))
+  # expect_error(gg(clv.neg), regexp = "negative prices")
 
   expect_silent(cdnow.neg[1000, Price := -1])
   expect_silent(clv.neg <- clvdata(cdnow.neg, date.format = "ymd", time.unit = "w", estimation.split = 37))
-  expect_error(gg(clv.neg), regexp = "negative")
+  expect_error(gg(clv.neg), regexp = "negative prices")
 })
 
 
