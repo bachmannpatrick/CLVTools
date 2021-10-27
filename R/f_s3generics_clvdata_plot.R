ec238421f0bac60571148e9204fe0ff0a6d34f46
Patrik <patrik.schilter@gmail.com>
2021-10-27 19:12:57 +0200

    plot histogram, incl remaining
    collect data, plot
    inputchecks
    docu

--- R/f_s3generics_clvdata_plot.R
+++ R/f_s3generics_clvdata_plot.R
@@ -1,27 +1,34 @@
 #' @title Plot Diagnostics for the Transaction data in a clv.data Object
 #'
 #' @param x The clv.data object to plot
-#' @param which Which plot to produce, either "tracking", "spending" or "interpurchasetime". May be abbreviated
-#' but only one may be selected. Defaults to "tracking".
-#' @param cumulative "tracking": Whether the cumulative actual repeat transactions should be plotted.
+#' @param which Which plot to produce, either "tracking", "numtrans", "spending" or "interpurchasetime".
+#' May be abbreviated but only one may be selected. Defaults to "tracking".
+#'
+#' @template template_param_verbose
 #' @param plot Whether a plot should be created or only the assembled data returned.
-#' @param sample Name of the sample for which the plot should be made, either
-#' "estimation", "full", or "holdout". Defaults to "estimation". Not for "tracking".
+#'
+#' @param cumulative "tracking": Whether the cumulative actual repeat transactions should be plotted.
 # @template template_param_predictionend
 #' @param prediction.end "tracking": Until what point in time to plot. This can be the number of periods (numeric) or
 #' a form of date/time object. See details.
-#' @template template_param_verbose
+#'
+#' @param trans.bins "numtrans": Vector of integers indicating the number of transactions (x axis) for which the customers should be counted.
+#' @param count.repeat.trans "numtrans": Whether repeat transactions (TRUE, default) or all transactions (FALSE) should be counted.
+#' @param count.remaining "numtrans": Whether the customers which are not captured with \code{trans.bins} should be counted in a separate last bar.
+#' @param label.remaining "numtrans": Label for the last bar, if \code{count.remaining=TRUE}.
+#'
 #' @param mean.spending "spending": Whether customer's mean spending per transaction (\code{TRUE}, default) or the
 #' value of every transaction in the data (\code{FALSE}) should be plotted.
+#'
+#' @param sample Name of the sample for which the plot should be made, either
+#' "estimation", "full", or "holdout". Defaults to "estimation". Not for "tracking".
 #' @param color Color of resulting geom object in the plot. Not for "tracking".
 #' @param geom The geometric object of ggplot2 to display the data. Forwarded to
 #' \link[ggplot2:stat_density]{ggplot2::stat_density}. Not for "tracking".
-#' @param ... Forwarded to \link[ggplot2:stat_density]{ggplot2::stat_density}. Not for "tracking".
+#' @param ... Forwarded to \link[ggplot2:stat_density]{ggplot2::stat_density} ("spending", "interpurchasetime")
+#' or \link[ggplot2:geom_bar]{ggplot2::geom_bar} ("numtrans"). Not for "tracking".
 #'
-#' @seealso \link[ggplot2:stat_density]{ggplot2::stat_density} for possible arguments to \code{...}
-#' @seealso \link[CLVTools:gg]{gg} to fit customer's average spending per transaction
-#' with the \code{Gamma-Gamma} model
-#' @seealso \link[CLVTools:plot.clv.fitted.transactions]{plot} to plot fitted transaction models
+#'
 #'
 #' @description
 #' Depending on the value of parameter \code{which}, one of the following plots will be produced:
@@ -29,6 +36,13 @@
 #' \subsection{Tracking Plot}{
 #' Plot the aggregated repeat transactions per period over the given time-horizon (\code{prediction.end}).
 #' See Details for the definition of plotting periods.
+#' }
+#'
+#' \subsection{Number of Transactions Plot}{
+# Plot distribution of the number of customers having the number of transactions.
+#' Plot the distribution of transactions or repeat transactions per customer, after aggregating transactions
+#' of the same customer on a single time point.
+#' Note that if \code{trans.bins} is changed, \code{label.remaining} usually needs to be adapted as well.
 #' }
 #'
 #' \subsection{Spending Plot}{
@@ -49,6 +63,13 @@
 #' is plotted. If the data is returned (i.e. with argument \code{plot=FALSE}), the respective rows
 #' contain \code{NA} in column \code{Number of Repeat Transactions}.
 #'
+#'
+#' @seealso \link[ggplot2:stat_density]{ggplot2::stat_density} for possible arguments to \code{...}
+#' @seealso \link[CLVTools:gg]{gg} to fit customer's average spending per transaction
+#' with the \code{Gamma-Gamma} model
+#' @seealso \link[CLVTools:plot.clv.fitted.transactions]{plot} to plot fitted transaction models
+#'
+#'
 #' @return
 #' An object of class \code{ggplot} from package \code{ggplot2} is returned by default.
 #' If \code{plot=FALSE}, the data that would have been used to create the plot is returned.
@@ -60,6 +81,8 @@
 #' \item{Spending}{Spending as defined by parameter \code{mean.spending}.}
 #' \item{mean.interpurchase.time}{Mean number of periods between transactions per customer,
 #' excluding customers with no repeat-transactions.}
+#' \item{num.transactions}{The number of (repeat) transactions, depending on \code{count.repeat.trans}.}
+#' \item{num.customers}{The number of customers.}
 #'
 #'
 #' @examples
@@ -88,6 +111,17 @@
 #' dt.plot.data <- plot(clv.cdnow, plot=FALSE)
 #'
 #'
+#' ### NUM CUSTOMERS WITH NUM TRANSACTION
+#' plot(clv.cdnow, which="numtrans", )
+#'
+#' # Count customers with
+#' #  Note that the bins are to be adapted to start from 1
+#' plot(clv.cdnow, which="numtrans", count.repeat.trans = FALSE,
+#'      trans.bins=1:10)
+#'
+#' # Do not plot
+#'
+#'
 #' ### SPENDING DENSITY
 #' # plot customer's average transaction value
 #' plot(clv.cdnow, which="spending", mean.spending = TRUE)
@@ -106,9 +140,12 @@
 #' @include all_generics.R class_clv_data.R
 #' @method plot clv.data
 #' @export
-plot.clv.data <- function(x, which=c("tracking", "spending", "interpurchasetime"),
+plot.clv.data <- function(x, which=c("tracking", "numtrans", "spending", "interpurchasetime"),
                           # tracking plot
                           prediction.end=NULL, cumulative=FALSE,
+                          # numtrans
+                          trans.bins=0:9, count.repeat.trans=TRUE, count.remaining=TRUE,
+                          label.remaining="10+",
                           # density
                           sample=c("estimation", "full", "holdout"),
                           geom="line", color="black",
@@ -121,12 +158,13 @@ plot.clv.data <- function(x, which=c("tracking", "spending", "interpurchasetime"
   err.msg <- c()
   err.msg <- c(err.msg, .check_user_data_single_boolean(b=plot, var.name="plot"))
   err.msg <- c(err.msg, .check_user_data_single_boolean(b=verbose, var.name="verbose"))
-  err.msg <- c(err.msg, .check_userinput_matcharg(char=which, choices=c("tracking", "spending", "interpurchasetime"),
+  err.msg <- c(err.msg, .check_userinput_matcharg(char=which, choices=c("tracking", "numtrans", "spending", "interpurchasetime"),
                                                   var.name="which"))
   check_err_msg(err.msg)
 
   return(
-    switch(EXPR = match.arg(arg=which, choices = c("tracking", "spending", "interpurchasetime"), several.ok = FALSE),
+    switch(EXPR = match.arg(arg=which, choices = c("tracking", "numtrans", "spending", "interpurchasetime"),
+                            several.ok = FALSE),
            "tracking" =
              clv.data.plot.tracking(x=x, prediction.end = prediction.end, cumulative = cumulative,
                                     plot = plot, verbose = verbose, ...=...),
@@ -137,8 +175,40 @@ plot.clv.data <- function(x, which=c("tracking", "spending", "interpurchasetime"
          "interpurchasetime" =
            clv.data.plot.density.interpurchase.time(clv.data = x, sample=sample,
                                                     plot=plot, verbose=verbose,
-                                                    color=color, geom=geom, ...)
+                                                    color=color, geom=geom, ...),
+         "numtrans" =
+           clv.data.plot.barplot.numtrans(clv.data = x, sample=sample,
+                                          trans.bins=trans.bins,
+                                          count.repeat.trans=count.repeat.trans,
+                                          label.remaining=label.remaining,
+                                          count.remaining=count.remaining,
+                                          plot=plot, verbose=verbose,
+                                          color=color, ...)
          ))
+}
+
+#' @importFrom ggplot2 rel
+clv.data.plot.add.theme <- function(p){
+  return(p + theme(
+    plot.title = element_text(face = "bold", size = rel(1.5)),
+    text = element_text(),
+    panel.background = element_blank(),
+    panel.border = element_blank(),
+    plot.background  = element_rect(colour = NA),
+    axis.title   = element_text(face = "bold",size = rel(1)),
+    axis.title.y = element_text(angle=90,vjust =2),
+    axis.title.x = element_text(vjust = -0.2),
+    axis.text = element_text(),
+    axis.line = element_line(colour="black"),
+    axis.ticks = element_line(),
+    panel.grid.major = element_line(colour="#d2d2d2"),
+    panel.grid.minor = element_blank(),
+    legend.key = element_blank(),
+    legend.position = "bottom",
+    legend.direction = "horizontal",
+    legend.title = element_text(face="italic"),
+    strip.background=element_rect(colour="#d2d2d2",fill="#d2d2d2"),
+    strip.text = element_text(face="bold", size = rel(0.8))))
 }
 
 
@@ -220,28 +290,7 @@ clv.data.make.density.plot <- function(dt.data, mapping, labs_x, title, geom, ..
   # Axis and title
   p <- p + labs(x = labs_x, y="Density", title=title)
 
-  p <- p + theme(
-    plot.title = element_text(face = "bold", size = rel(1.5)),
-    text = element_text(),
-    panel.background = element_blank(),
-    panel.border = element_blank(),
-    plot.background  = element_rect(colour = NA),
-    axis.title   = element_text(face = "bold",size = rel(1)),
-    axis.title.y = element_text(angle=90,vjust =2),
-    axis.title.x = element_text(vjust = -0.2),
-    axis.text = element_text(),
-    axis.line = element_line(colour="black"),
-    axis.ticks = element_line(),
-    panel.grid.major = element_line(colour="#d2d2d2"),
-    panel.grid.minor = element_blank(),
-    legend.key = element_blank(),
-    legend.position = "bottom",
-    legend.direction = "horizontal",
-    legend.title = element_text(face="italic"),
-    strip.background=element_rect(colour="#d2d2d2",fill="#d2d2d2"),
-    strip.text = element_text(face="bold", size = rel(0.8)))
-
-  return(p)
+  return(clv.data.plot.add.theme(p))
 }
 
 #' @importFrom ggplot2 aes
@@ -303,3 +352,85 @@ clv.data.plot.density.interpurchase.time <- function(clv.data, sample,
     return(dt.mean.interp)
   }
 }
+
+
+#' @importFrom ggplot2 ggplot geom_col aes_string labs geom_text position_dodge rel
+clv.data.plot.barplot.numtrans <- function(clv.data, count.repeat.trans, count.remaining, label.remaining, trans.bins,
+                                           sample, plot, verbose, color, ...){
+  x <- num.customers <- num.transactions <- NULL
+
+  err.msg <- c()
+  err.msg <- c(err.msg, .check_user_data_single_boolean(b=count.repeat.trans, var.name="count.repeat.trans"))
+  err.msg <- c(err.msg, .check_user_data_single_boolean(b=count.remaining,    var.name="count.remaining"))
+  err.msg <- c(err.msg, .check_userinput_single_character(char=label.remaining, var.name="label.remaining"))
+  err.msg <- c(err.msg, check_user_data_emptyellipsis(...))
+  check_err_msg(err.msg) # count.repeat.trans has to be checked first
+  check_err_msg(check_userinput_datanocov_transbins(trans.bins=trans.bins, count.repeat.trans=count.repeat.trans))
+
+
+  # Number of customers which have the given number of transactions
+  trans.bins <- unique(trans.bins)
+  dt.bins <- data.table(x = trans.bins)
+
+  dt.trans <- clv.data.select.sample.data(clv.data=clv.data, sample=sample, choices=c("estimation", "full", "holdout"))
+
+  dt.num.trans <- dt.trans[, list(x = .N), by="Id"]
+
+  if(count.repeat.trans){
+    dt.num.trans[, x := x-1]
+    x.lab <- "Number of Repeat Transactions"
+  }else{
+    x.lab <- "Number of Transactions"
+  }
+
+  # count how many customers had how many num trans
+  dt.num.cust.by.trans <- dt.num.trans[, list(num.customers = .N), by="x"]
+
+  # add how many customers made x num trans
+  #   NA where does not match, set 0 to keep
+  dt.bins <- dt.bins[dt.num.cust.by.trans, num.customers := num.customers, on="x"]
+  dt.bins[is.na(num.customers), num.customers := 0]
+  setnames(dt.bins, "x", "num.transactions")
+  # Make char to bind together with "remaining"
+  #   get levels order before turning into character
+  levels.bins <- dt.bins[, sort(num.transactions)]
+  dt.bins[, num.transactions := as.character(num.transactions)]
+
+  if(count.remaining){
+    # find all num.trans which are not already in bins
+    dt.remaining <- dt.num.cust.by.trans[!(x %in% dt.bins[, as.numeric(num.transactions)])]
+    dt.remaining <- dt.remaining[, list(num.transactions=label.remaining, num.customers = sum(num.customers))]
+    dt.bins <- rbindlist(list(dt.bins, dt.remaining))
+    # add remaining as last/highest level
+    levels.bins <- c(levels.bins, label.remaining)
+  }
+
+  # turn into ordered category for plotting
+  dt.bins[, num.transactions := factor(num.transactions, levels = levels.bins, ordered = TRUE)]
+
+  if(!plot){
+    return(dt.bins)
+  }else{
+    #   use geom_col because geom_bar does the counting itself
+    p <- ggplot(data = dt.bins) + geom_col(mapping = aes_string(x="num.transactions",
+                                                                y="num.customers"),
+                                           fill=color,
+                                           width = 0.5,
+                                           show.legend = FALSE)
+    # Axis and title
+    p <- p + labs(x = x.lab, y="Number of Customers",
+                  # Number of Customers per Number of Transactions
+                  title="Distribution of Transaction Count")
+
+    # add count annotation
+    p <- p + geom_text(aes_string(label = "num.customers",
+                                  x = "num.transactions", y = "num.customers"),
+                       position = position_dodge(width = 0.8),
+                       vjust = -0.6,
+                       size = rel(3))
+
+    return(clv.data.plot.add.theme(p))
+  }
+}
+
+
