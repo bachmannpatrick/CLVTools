7f5082ae76795c5414f5c4f3b90e95d2c12fd5a4
Patrik <patrik.schilter@gmail.com>
2021-11-02 18:12:13 +0100

    fix R CMD checks

--- man-roxygen/template_clvfittedtransactions_seealso.R
+++ man-roxygen/template_clvfittedtransactions_seealso.R
@@ -5,6 +5,6 @@
 #'
 #' @seealso \code{\link[CLVTools:predict.clv.fitted.transactions]{predict}} to predict expected transactions, probability of being alive, and customer lifetime value for every customer
 #' @seealso \code{\link[CLVTools:plot.clv.fitted.transactions]{plot}} to plot the unconditional expectation as predicted by the fitted model
-#' @seealso \code{\link[CLVTools:pmf.clv.fitted.transactions]{pmf}} for the probability to make exactly x transactions in the estimation period, given by the probability mass function (PMF).
+#' @seealso \code{\link[CLVTools:pmf]{pmf}} for the probability to make exactly x transactions in the estimation period, given by the probability mass function (PMF).
 #' @seealso The generic functions \code{\link[CLVTools:vcov.clv.fitted]{vcov}}, \code{\link[CLVTools:summary.clv.fitted]{summary}}, \code{\link[CLVTools:fitted.clv.fitted]{fitted}}.
 #'
