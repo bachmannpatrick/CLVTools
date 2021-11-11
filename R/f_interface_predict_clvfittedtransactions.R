0a0479e82ccdb899d82959ac79bc6835d1c78807
Patrik <patrik.schilter@gmail.com>
2021-11-11 19:20:59 +0100

    discount rate for non annual time units

--- R/f_interface_predict_clvfittedtransactions.R
+++ R/f_interface_predict_clvfittedtransactions.R
@@ -5,7 +5,9 @@
 #' @param object A fitted clv transaction model for which prediction is desired.
 #' @param newdata A clv data object for which predictions should be made with the fitted model. If none or NULL is given, predictions are made for the data on which the model was fit.
 #' @param predict.spending Whether and how to predict spending and based on it also CLV, if possible. See details.
-#' @param continuous.discount.factor continuous discount factor to use to calculate \code{DERT/DECT}
+#' @param continuous.discount.factor The continuous discount factor used to calculate \code{DERT/DECT}, defaults
+#' to \code{0.1} (=10\%). See details for how to calculate it appropriately.
+#'
 #' @template template_param_predictionend
 #' @template template_param_verbose
 #' @template template_param_dots
@@ -20,7 +22,8 @@
 #' \item "discounted expected residual transactions" (\code{DERT}) for every customer, which is the total number of
 #' transactions for the residual lifetime of a customer discounted to the end of the estimation period.
 #' In the case of time-varying covariates, instead of \code{DERT}, "discounted expected conditional transactions" (\code{DECT})
-#' is predicted. \code{DECT} does only cover a finite time horizon in contrast to \code{DERT}. For \code{continuous.discount.factor=0}, \code{DECT} corresponds to \code{CET}.
+#' is predicted. \code{DECT} does only cover a finite time horizon in contrast to \code{DERT}.
+#' For \code{continuous.discount.factor=0}, \code{DECT} corresponds to \code{CET}.
 #'}
 #'
 #' In order to derive a monetary value such as CLV, customer spending has to be considered.
@@ -42,9 +45,10 @@
 #'
 #' @template template_details_predictionend
 #'
-#' @details \code{continuous.discount.factor} allows to adjust the discount rate used to estimated the discounted expected
-#' transactions (\code{DERT/DECT}).
-#' The default value is \code{0.1} (=10\%). Note that a continuous rate needs to be provided.
+#' @details \code{continuous.discount.factor} is the continuous rate used to discount the expected residual
+#' transactions (\code{DERT/DECT}). An annual rate of (100 x d)\% equals a continuous rate delta = ln(1+d).
+#' To account for time units which are not annual, the continuous rate has to be further adjusted
+#' to delta=ln(1+d)/k, where k are the number of time units in a year.
 #'
 #'
 #' @seealso models to predict transactions: \link{pnbd}, \link{bgnbd}, \link{ggomnbd}.
