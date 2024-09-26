4f56d8cde53f80a8919088ba4b1e880d4ecd6057
Patrik Schilter <patrik.schilter@gmail.com>
2024-09-26 22:05:38 +0200

    Bootstrapping: Regular Quantiles (#273)
    * Calculate CI boundaries as "regular quantiles" rather than "reverse quantiles" because reversed quantiles might be negative.
    * Update docu: Remove all references to "reversed quantiles"

--- R/f_interface_predict_clvfittedtransactions.R
+++ R/f_interface_predict_clvfittedtransactions.R
@@ -80,12 +80,21 @@
 #' These provide an estimate of parameter uncertainty.
 #' To create bootstrapped data, customer ids are sampled with replacement until reaching original
 #' length and all transactions of the sampled customers are used to create a new \code{clv.data} object.
-#' A new model is fit on the bootstrapped data with the same specification as \code{object}
-#' (incl. start parameters and `optimx.args`) and it is then used to predict on this data.
+#' A new model is fit on the bootstrapped data with the exact same specification as used when
+#' fitting \code{object} (incl. start parameters and `optimx.args`) and it is then used to predict on this data.
+#'
+#' It is highly recommended to fit the original model (\code{object}) with a robust optimization
+#' method, such as Nelder-Mead (\code{optimx.args=list(method='Nelder-Mead')}).
+#' This ensures that the model can also be fit on the bootstrapped data.
+#'
 #' All prediction parameters, incl \code{prediction.end} and \code{continuous.discount.factor}, are forwarded
 #' to the prediction on the bootstrapped data.
-#' Per customer, confidence intervals of each predicted metric are created using a "reversed quantile" approach.
+#' Per customer, the boundaries of the confidence intervals of each predicted metric are the
+#' sample quantiles (\code{quantile(x, probs=c((1-level)/2, 1-(1-level)/2)}).
+#'
 #' See \link{clv.bootstrapped.apply} to create a custom bootstrapping procedure.
+#'
+
 #'
 #'
 #' @seealso models to predict transactions: \link{pnbd}, \link{bgnbd}, \link{ggomnbd}.
