9ffcd3a47e40e688ca15620f9f530297c9bcce01
Patrik Schilter <patrik.schilter@gmail.com>
2025-08-14 00:22:31 +0200

    Fix docu: parameter name

--- man-roxygen/template_params_clvdata.R
+++ man-roxygen/template_params_clvdata.R
@@ -1,6 +1,6 @@
 #' @param date.format Character string that indicates the format of the date variable in the data used. See details.
 #' @param time.unit What time unit defines a period. May be abbreviated, capitalization is ignored. See details.
-#' @param observation.split The end of the observation period, beyond the last recorded transaction in \code{<%=name_param_trans%>}. See details.
+#' @param observation.end The end of the observation period, beyond the last recorded transaction in \code{<%=name_param_trans%>}. See details.
 #' @param estimation.split Indicates the length of the estimation period. See details.
 #' @param name.id Column name of the customer id in \code{<%=name_param_trans%>}.
 #' @param name.date Column name of the transaction date in \code{<%=name_param_trans%>}.
